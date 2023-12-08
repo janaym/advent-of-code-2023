8e03bf1fb477c21e6c9e3cb93564e679e38853e9
Janay Ma <janay.ma7@gmail.com>
2023-12-08 01:33:06 -0800

    check for spelled digits

--- day_01/problem_01/__init__.py
+++ day_01/problem_01/__init__.py
@@ -15,3 +15,20 @@ def getCalibrationValue(str):
 
   return int(calibrationValue)
 
+#checks a string for spelled digits and returns an array containing 
+def check_for_spelled_digits(str):
+
+  string_fragments = [];
+
+  spelled_digits = [{'one': 1}, {'two': 2}, {'three': 3}, {'four': 4}, {'five': 5}, {'six': 6}, {'seven': 7}, {'eight': 8}, {'nine': 9}]
+
+  for spelling in spelled_digits:
+    spelling_start_index = str.find(spelling)
+    spelling_end_index = i + str.len() + 1
+    string_fragments.append((str[0:spelling_start_index], str[spelling_end_index:-1]))
+  
+  return string_fragments
+
+
+
+
