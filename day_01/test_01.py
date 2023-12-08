bc94cb32c5a2cae4bd8f73f59e8e608b6190ded5
Janay Ma <janay.ma7@gmail.com>
2023-12-08 00:27:59 -0800

    Write tests

--- day_01/test_01.py
+++ day_01/test_01.py
@@ -2,6 +2,13 @@ import unittest
 from problem_01 import getCalibrationValues
 
 class TestGetCalibrationValues(unittest.TestCase):
+
+  def test_random_string(self):
+    '''
+    check it works without edge case
+    '''
+    self.assertEqual(getCalibrationValues('pqr3stu8vwx'), 38)
+
   def test_list_digit(self):
     '''
     test it can handle a list of integers
@@ -10,6 +17,19 @@ class TestGetCalibrationValues(unittest.TestCase):
     data = '123'
     result = getCalibrationValues(data)
     self.assertEqual(result, 13)
+
+  def test_no_digits(self):
+    '''
+    test when no digits, returns zero
+    '''
+    self.assertEqual(getCalibrationValues('abcdef'), 0)
+
+  def test_one_digit(self):
+    '''
+    test when only one digit N present, returns NN
+    '''
+    self.assertEqual(getCalibrationValues('treb7uchet'), 77)
+
 
 if __name__ == '__main__':
   unittest.main()
