2c64c0b767746baa6b0b8a72e47fed1b3e0b0bf9
Janay Ma <janay.ma7@gmail.com>
2023-12-08 13:11:58 -0800

    Implement recursive search for spelled digit

--- day_01/problem_01/__init__.py
+++ day_01/problem_01/__init__.py
@@ -18,22 +18,38 @@ def getCalibrationValue(str):
 
 # takes in string, converts any spelled digit to a numerical digit
 def convert_spelled_digits(str):
+  print('converting')
     
   spelled_digits = [('one', '1'), ('two', '2'), ('three', '3'), ('four', '4'), ('five', '5'), ('six', '6'), ('seven', '7'), ('eight', '8'), ('nine', '9')]
 
   new_str = str
 
   for spelling_pair in spelled_digits:
-    spelling = spelling_pair[0]
-    i = new_str.find(spelling)
-    j = -1
-    #if spelling found:
-    if i != -1: 
-      j = i + len(spelling) 
-      new_str = new_str[:i] + spelling_pair[1] + new_str[j:]
+    print ('before converting 1')
+    new_str = convert_one_spelling(new_str, spelling_pair)
+    print ('after', new_str)
       
     
   return new_str
+
+
+def convert_one_spelling(str, spelling_pair):
+  print('in one spelling with spelling pair:', spelling_pair)
+  spelling = spelling_pair[0]
+  i = str.find(spelling)
+  j = -1
+  print('current string is ', str)
+  new_str = str
+
+  if i != -1:
+    j = i + len(spelling)
+    new_str = new_str[:i] + spelling_pair[1] + new_str[j:]
+    print(new_str)
+    new_str = convert_one_spelling(new_str, spelling_pair)
+
+  print('returning string from recurision', new_str, i, j)
+  return new_str
+
 
 
 
