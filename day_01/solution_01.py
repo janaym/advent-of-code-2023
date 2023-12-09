145d5fcb8f44baf9e69398943f59990af091e427
Janay Ma <janay.ma7@gmail.com>
2023-12-09 01:32:26 -0800

    more day 1 work

--- day_01/solution_01.py
+++ day_01/solution_01.py
@@ -1,13 +1,18 @@
 from problem_01 import get_calibration_value, convert_spelled_digits
 
+
 with open("/Users/janayma/Documents/advent-of-code-2023/day_01/input.txt", 'r') as f:
   sum_values = 0
 
   for line in f:
-    sum_values += get_calibration_value(convert_spelled_digits(line))
+    converted_data = convert_spelled_digits(line)
+    sum_values += get_calibration_value(converted_data[0], converted_data[1])
 
   f.close()
 
 print("The total sum of the calibration values is:", sum_values)
+
+
+# convert_spelled_digits('one')
 
 
--- day_01/test_01.py
+++ day_01/test_01.py
@@ -1,5 +1,5 @@
 import unittest
-from problem_01 import get_calibration_value
+from problem_01 import get_calibration_value, convert_spelled_digits
 
 class TestGetCalibrationValues(unittest.TestCase):
 
@@ -8,7 +8,7 @@ class TestGetCalibrationValues(unittest.TestCase):
     check it works without edge case
     '''
     data = 'pqr3stu8vwx'
-    result = get_calibration_value(data)
+    result = get_calibration_value(data, reversed(data))
     self.assertEqual(result, 38)
 
   def test_list_digit(self):
@@ -16,27 +16,38 @@ class TestGetCalibrationValues(unittest.TestCase):
     test it can handle a list of integers
     '''
     data = '123456'
-    result = get_calibration_value(data)
+    result = get_calibration_value(data, reversed(data))
     self.assertEqual(result, 16)
 
   def test_no_digits(self):
     '''
     test when no digits, returns zero
     '''
-    self.assertEqual(get_calibration_value('abcdef'), 0)
+    data = 'abcdef'
+    self.assertEqual(get_calibration_value('abcdef', reversed(data)), 0)
 
   def test_one_digit(self):
     '''
     test when only one digit N present, returns NN
     '''
-    self.assertEqual(get_calibration_value('treb7uchet'), 77)
+    data = 'tre7uchet'
+    self.assertEqual(get_calibration_value(data, reversed(data)), 77)
 
   def test_empty_string(self):
     '''
     test it can handle an empty string
     '''
-    self.assertEqual(get_calibration_value(''), 0)
+    self.assertEqual(get_calibration_value('', ''), 0)
+    
+class TestConvertSpelledDigits(unittest.TestCase):
+  def test_non_problematic(self):
+    self.assertEqual(convert_spelled_digits('abcdef123'), ('abcdef123', '321fedcba'))
+  
+  def test_spelled_digits(self):
+    self.assertEqual(convert_spelled_digits('one'), ('1', '1'))
 
+  def test_overlapping_digits(self):
+    self.assertEqual(convert_spelled_digits('twone'), ('2ne', 'tw1'))
 
 if __name__ == '__main__':
   unittest.main()
