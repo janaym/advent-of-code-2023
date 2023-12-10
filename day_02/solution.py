c745147effab45884cd9bebb72448e238e15b306
Janay Ma <janay.ma7@gmail.com>
2023-12-09 21:59:40 -0800

    solve question 2b

--- day_02/solution.py
+++ day_02/solution.py
@@ -2,12 +2,6 @@ import re
 
 cube_count = {'red': 12, 'green': 13, 'blue': 14} #red, green, blue
 
-
-
-str = 'Game 100: 9 blue, 4 red; 5 green, 10 blue, 11 red; 1 green, 1 red; 1 green, 5 red, 1 blue'
-print(re.search(r'(\d+)', str).group(1)) 
-
-
 def is_game_possible(str, cube_count):
   pattern = r'(\d+)\s(\w+)'
 
@@ -18,28 +12,56 @@ def is_game_possible(str, cube_count):
     count = match.group(1)
 
     if cube_count[color] < int(count):
-      print('found color count too high:', color, count)
       return False
 
     str = re.split(r'\d+\s\w+', str, maxsplit=1)[1]
     match = re.search(pattern, str)
 
   return True
+
+def get_min_cube_set(str):
+  pattern = r'(\d+)\s(\w+)'
+  highest_color_counts = {'red': 0, 'blue': 0, 'green': 0}
+
+  match = re.search(pattern, str)
+  while match:
+    
+    color = match.group(2)
+    count = int(match.group(1))
+
+    if highest_color_counts[color] < count:
+      highest_color_counts[color] = count
+    
+    str = re.split(r'\d+\s\w+', str, maxsplit=1)[1]
+    match = re.search(pattern, str)
   
+  return highest_color_counts
 
-print(is_game_possible(str, cube_count))
-
+print(get_min_cube_set(str))
 
 with open("/Users/janayma/Documents/advent-of-code-2023/day_02/input.txt", 'r') as f:
+
+  #2a
   id_sum = 0
+  power_sum = 0
   for line in f:
-    #get id:
+    #part 1
     game_id = int(re.search(r'(\d+)', line).group(1))
     if (is_game_possible(line, cube_count)):
       id_sum += game_id
 
-  print('the sum of ids is:', id_sum)
+    #part 2
+    power = 1
+    min_set = get_min_cube_set(line)
+    for color in min_set:
+      power *= min_set[color]
 
+    power_sum += power
+
+  print('the sum of ids is:', id_sum)
+  print('the sume of all game powers is: ', power_sum)
+
+    
   f.close()
 
 
