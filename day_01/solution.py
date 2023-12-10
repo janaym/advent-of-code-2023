d2a681d26b59502626eb7151a6c990ffb4f4aa39
Janay Ma <janay.ma7@gmail.com>
2023-12-09 23:39:20 -0800

    solve 1b

--- day_01/solution.py
+++ day_01/solution.py
@@ -1,7 +1,6 @@
 import re
 
-test_str = 'bfjtdslkdbthree4jvvonezqdthreesrghnnb6six'
-
+# soln for part 1
 def get_calbration_value(str):
   calibration_value = ''
 
@@ -20,13 +19,55 @@ def get_calbration_value(str):
   
   return calibration_value
 
-print(get_calbration_value(test_str))
+# soln for part 2
+def get_calbration_value_2(str):
+  calibration_value = ''
+
+  digit_spellings_dict = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+  }
+  
+  # search forwards
+  pattern = r'(\d|' + '|'.join(digit_spellings_dict.keys()) + ')'
+  first_digit = re.search(pattern, str).group(1)
+  if first_digit.isnumeric():
+    calibration_value += first_digit
+  else:
+    first_digit = digit_spellings_dict[first_digit]
+    calibration_value += first_digit
+
+  # setup reverse string searching dictionary and pattern
+  reversed_spellings_dict = {key[::-1]: value for key, value in digit_spellings_dict.items()}
+  reverse_pattern = pattern = r'(\d|' + '|'.join(reversed_spellings_dict.keys()) + ')'
+
+  
+  # search in reverse
+  last_digit = re.search(reverse_pattern, str[::-1]).group(1)
+  if last_digit.isnumeric():
+    calibration_value += last_digit
+  else:
+    calibration_value += reversed_spellings_dict[last_digit]
+
+  return calibration_value
+
+
+
 
 with open("/Users/janayma/Documents/advent-of-code-2023/day_01/input.txt", 'r') as f:
+  # using solution no.2
   values_sum = 0
 
   for line in f:
-    calibration_value = get_calbration_value(line)
+    calibration_value = get_calbration_value_2(line)
     values_sum += int(calibration_value)
 
   print('the sum of calibration values is', values_sum)
+  f.close()
